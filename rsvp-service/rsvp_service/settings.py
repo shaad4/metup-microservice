d948e4e2022063c3c2a6c012acd1f630839f0cb5
shaad4 <mohammedshaadn@gmail.com>
2026-07-28 16:31:14 +0530

    feat(rsvp-service): add RSVP model and initial migration, configure CORS headers

--- rsvp-service/rsvp_service/settings.py
+++ rsvp-service/rsvp_service/settings.py
@@ -37,9 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'corsheaders',
+
+    'rsvps',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -115,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
--- rsvp-service/rsvps/models.py
+++ rsvp-service/rsvps/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class RSVP(models.Model):
+    STATUS_CHOICES = [
+        ('joined', 'Joined'),
+        ('cancelled', 'Cancelled'),
+    ]
+
+    event_id = models.IntegerField()
+    user_id = models.IntegerField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='joined')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('event_id', 'user_id')
+
+    def __str__(self):
+        return f"user={self.user_id} event={self.event_id} status={self.status}"
