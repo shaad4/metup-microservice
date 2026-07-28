877f6fac2806997d77f96c9e07181d45db03c852
shaad4 <mohammedshaadn@gmail.com>
2026-07-28 12:59:47 +0530

    feat(event-service): define   Event model, add dependencies,   and generate initial migrations

--- event-service/events/models.py
+++ event-service/events/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    location = models.CharField(max_length=200)
+    start_time = models.DateTimeField()
+    capacity = models.PositiveIntegerField()
+    created_by = models.IntegerField() 
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
